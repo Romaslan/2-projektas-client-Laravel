83e0942e2172b46d866095677665e5680aea07f1
Romaslan <93395762+Romaslan@users.noreply.github.com>
2022-01-22 16:00:25 +0200

    *types, model, seeders

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,16 @@ Route::prefix('companies')->group(function() {
 
 
 });
+
+//Types
+Route::prefix('types')->group(function() {
+
+    Route::get('', 'App\Http\Controllers\TypeController@index')->name('type.index');
+    Route::get('create', 'App\Http\Controllers\TypeController@create')->name('type.create');
+    Route::post('store', 'App\Http\Controllers\TypeController@store' )->name('type.store');
+    Route::get('edit/{type}', 'App\Http\Controllers\TypeController@edit')->name('type.edit');
+    Route::post('update/{type}', 'App\Http\Controllers\TypeController@update')->name('type.update');
+    Route::post('destroy/{type}', 'App\Http\Controllers\TypeController@destroy' )->name('type.destroy');
+    Route::get('show/{type}', 'App\Http\Controllers\TypeController@show')->name('type.show');
+
+});
